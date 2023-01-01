3f39ed9dc694b1c24061e4c0ca521bf2154a4031
= <=>
2023-01-01 15:06:34 +0530

    add

---
+++ index.js
@@ -0,0 +1,27 @@
+// var a = ["a","b","c","a","d","e","e","c","f","b"];
+// var b = new Set(a);
+
+// console.log(b);
+// function countAlphabet(s)
+// {
+//   var t = new Map();
+ 
+//   s.split('').forEach(element => {
+     
+//         if(t.has(element))
+//         {
+//             t.set(element, d.get(element)+1);
+//         }
+//         else
+//             t.set(element, 1);
+//   });
+ 
+//   s.split('').forEach(element => {
+//     if(t.has(element) && t.get(element)!=0)
+//     {
+//       console.log( element + "=" + t.get(element) + " ");
+//       t.set(element, 0);
+//     }
+//   });
+// }
+// countAlphabet("abcadeecfb"); 
